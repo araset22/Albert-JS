f5de6aeda1ed8816784d350355488fcf79f8661c
Albert Raset <albert.raset@institutvidreres.cat>
2021-11-23 09:02:36 +0100

    Exercici 6

---
+++ JS_Variables_AlbertRaset.js
@@ -0,0 +1,10 @@
+var x = 1234
+var y = 5678
+var suma = x + y
+var resta = x - y
+var mult = x * y
+var div = x / y
+console.log(suma.toFixed(1))
+console.log(resta.toFixed(1))
+console.log(mult.toFixed(1))
+console.log(div.toFixed(1))
